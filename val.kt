1a36cd63baa82d3b10071d15dc4bd14e9f1c3d6c
ronnyzho <ronnyzhong@yahoo.com>
2025-04-07 23:25:06 -0600

    Create val.kt

---
+++ val.kt
@@ -0,0 +1,18 @@
+
+fun main (){
+    val myNum: Int = 5                // Int
+    val myDoubleNum: Double = 5.99    // Double
+    val myBoolean: Boolean = true     // Boolean
+    val boolean: Boolean = false    // Boolean
+    val myText: String = "Hello"      // String
+    val myFloat: Float = 19.99f       // Float
+    val myLong: Long = 150000   // Long
+    val myShort: Short = 5000         // Short
+    val myByte: Byte? = 100            // Byte
+    val myLetter: Char? = 'D'          // Char
+}
+
+fun subtracts(a: Int,b: Int): String {
+    var s: String = "The result is: "
+    return a - b
+ }
