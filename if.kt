f9da3a85822a35c908931bd3fa5fc925a56a1957
missymachine123 <arianams2049@gmail.com>
2025-05-08 11:28:05 -0600

    funcall and params working

--- if.kt
+++ if.kt
@@ -1,10 +1,9 @@
-fun func(n : Int) : Int {
-    
-}
+val X : Int = 2;
+val Z : Double = 3.14;
+
 fun main() {
-  var i : Int = 5;
-  var b : Int = 6;
-  var d : Int = 8;
-  func(i);
-  println(i-1)
-}
+  var  i : Int = 5;
+  var  y : String = "x" + "";
+  var  k : Double = 3.14;
+  println("$i");
+  }
